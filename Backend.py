871cf95ef31aa7721c2dc8d68503481b3371380c
42yo <kqlys@yandex.ru>
2023-05-27 11:58:42 +0300

    Accidentally created file

--- Backend.py
+++
@@ -1,7 +0,0 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
-from MainWindow import Ui_MainWindow
-class Ui_MainWindow(object):
-    def add_label(self, MainWindow):
-        print("add")
-    def add_logics(self, MainWindow):
-        self.p2.clicked.connect(self.add_label)
