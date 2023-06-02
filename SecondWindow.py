6af332b5a45ffd5fbfbaa2d1b6d960b2b7d855fb
42yo <kqlys@yandex.ru>
2023-06-03 02:43:29 +0300

    Внедрена работа кнопок "Save" и "Remove"

--- SecondWindow.py
+++ SecondWindow.py
@@ -9,30 +9,51 @@ class Ui_SecondWindow(object):
         SecondWindow.resize(400, 600)
         self.centralwidget = QtWidgets.QWidget(SecondWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.listWidget = QtWidgets.QListWidget(self.centralwidget)
-        self.listWidget.setGeometry(QtCore.QRect(50, 210, 300, 370))
-        self.listWidget.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
-        self.listWidget.setObjectName("listWidget")
         self.timeEdit = QtWidgets.QTimeEdit(self.centralwidget)
-        self.timeEdit.setGeometry(QtCore.QRect(120, 20, 160, 40))
+        self.timeEdit.setGeometry(QtCore.QRect(260, 60, 70, 40))
         self.timeEdit.setObjectName("timeEdit")
         self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
-        self.textEdit.setGeometry(QtCore.QRect(120, 80, 160, 50))
+        self.textEdit.setGeometry(QtCore.QRect(70, 60, 160, 130))
         self.textEdit.setObjectName("textEdit")
+        self.listWidget = QtWidgets.QListWidget(self.centralwidget)
+        self.listWidget.setGeometry(QtCore.QRect(50, 210, 300, 285))
+        self.listWidget.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
+        self.listWidget.setObjectName("listWidget")
         self.button_for_adding = QtWidgets.QPushButton(self.centralwidget)
-        self.button_for_adding.setGeometry(QtCore.QRect(140, 146, 120, 40))
+        self.button_for_adding.setGeometry(QtCore.QRect(250, 120, 90, 70))
+        font = QtGui.QFont()
+        font.setFamily("Comic Sans MS")
+        font.setPointSize(14)
+        font.setBold(False)
+        font.setWeight(50)
+        self.button_for_adding.setFont(font)
         self.button_for_adding.setObjectName("button_for_adding")
+        self.button_for_save = QtWidgets.QPushButton(self.centralwidget)
+        self.button_for_save.setGeometry(QtCore.QRect(60, 520, 110, 50))
+        font = QtGui.QFont()
+        font.setFamily("MS Sans Serif")
+        font.setPointSize(14)
+        self.button_for_save.setFont(font)
+        self.button_for_save.setObjectName("button_for_save")
+        self.button_for_removing = QtWidgets.QPushButton(self.centralwidget)
+        self.button_for_removing.setGeometry(QtCore.QRect(230, 520, 110, 50))
+        font = QtGui.QFont()
+        font.setFamily("MS Sans Serif")
+        font.setPointSize(14)
+        self.button_for_removing.setFont(font)
+        self.button_for_removing.setObjectName("button_for_removing")
         self.Backgrounddd = QtWidgets.QLabel(self.centralwidget)
         self.Backgrounddd.setGeometry(QtCore.QRect(0, 0, 400, 600))
         self.Backgrounddd.setText("")
         self.Backgrounddd.setPixmap(QtGui.QPixmap("D:/Загрузки/background_of_diary.jpg"))
-
         self.Backgrounddd.setObjectName("Backgrounddd")
         self.Backgrounddd.raise_()
-        self.listWidget.raise_()
         self.timeEdit.raise_()
         self.textEdit.raise_()
+        self.listWidget.raise_()
         self.button_for_adding.raise_()
+        self.button_for_save.raise_()
+        self.button_for_removing.raise_()
         SecondWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(SecondWindow)
@@ -43,17 +64,21 @@ class Ui_SecondWindow(object):
     def retranslateUi(self, SecondWindow):
         '''Устанавливает текст и заголовки виджетов.'''
         _translate = QtCore.QCoreApplication.translate
-        SecondWindow.setWindowTitle(_translate("SecondWindow", "Timetable"))
+        SecondWindow.setWindowTitle(_translate("SecondWindow", "Write_there_a_datatime"))
         __sortingEnabled = self.listWidget.isSortingEnabled()
         self.listWidget.setSortingEnabled(False)
         self.listWidget.setSortingEnabled(__sortingEnabled)
         self.button_for_adding.setText(_translate("SecondWindow", "Add"))
+        self.button_for_save.setText(_translate("SecondWindow", "Save"))
+        self.button_for_removing.setText(_translate("SecondWindow", "Delete"))
 
     def add_functions(self, today_date):
         '''Добавляет все необходимые функции для корректной работы приложения.'''
         self.update_tasks(today_date)
 
         self.button_for_adding.clicked.connect(lambda: self.add_task(self.textEdit.toPlainText(), today_date))
+        self.button_for_save.clicked.connect(lambda: self.save_changes(today_date))
+        self.button_for_removing.clicked.connect(lambda: self.remove_task(today_date))
 
     def add_task(self, new_task, date):
         '''Добавляет задачу в базу данных.'''
@@ -79,8 +104,26 @@ class Ui_SecondWindow(object):
             cursor.execute(query, row)
             db.commit()
 
-            self.save_changes(date)
             self.update_tasks(date)
+            self.textEdit.clear()
+
+    def remove_task(self, date):
+        '''Удаляет задачу'''
+        db = sqlite3.connect("data.db")
+        cursor = db.cursor()
+
+        removing_item = self.listWidget.currentItem()
+        removing_task = removing_item.text()
+        removing_completed = 1 if removing_item.checkState() == QtCore.Qt.Checked else 0
+
+        query1 = "SELECT id FROM Tasks WHERE task = ? and completed = ? and date = ?"
+        row1 = (removing_task, removing_completed, date, )
+        result_id = cursor.execute(query1, row1).fetchone()
+
+        cursor.execute("DELETE from Tasks WHERE id = ?", (result_id[0], ))
+        db.commit()
+
+        self.update_tasks(date)
 
     def update_tasks(self, date):
         '''Обновляет список с задачами из базы данных.'''
